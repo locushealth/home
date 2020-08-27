f0bfc28b1ba3d7641ab2714d0c8699278438ac1d
keshav-bajaj <66621502+keshav-bajaj@users.noreply.github.com>
2020-08-27 17:03:39 +0530

    Update form1.php

--- form1.php
+++ form1.php
@@ -1,28 +1,15 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
-
-    $email_from = "vasudhagroup.3@gmail.com";
 
     $email_subject = "New Form Submission";
 
-    $email_body = "User Name: $name.\n".
-                   "User Email: $visitor_email.\n".
-                   "User Message: $message.\n";
+    $email_body = "hello";
 
 
-    $headers = "From $email_from \r\n";
-
-    $headers = "Reply-To: $visitor_email \r\n";
-
-    if(mail("vasudhagroup.3@gmail.com",$email_subject,$email_body,$headers))
+    if(mail("vasudhagroup.3@gmail.com",$email_subject,$email_body,))
     {
       $msg = "Message Sent";
 
     echo $msg;
   }
     }
-
-    header("Location: index.html");
 ?>
