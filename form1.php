65640410741579ccfdb657b54cf9b7a3b85de6d4
keshav-bajaj <66621502+keshav-bajaj@users.noreply.github.com>
2020-08-28 16:36:20 +0530

    Update form1.php

--- form1.php
+++ form1.php
@@ -1,13 +1,7 @@
 <?php
-$to = "vasudhagroup.3@gmail.com";
-$subject = "lol";
-$message = "lol2";
-$headers = "Frome:vasudhagroup.3@gmail.com";
 
-if (mail("vasudhagroup.3@gmail.com",$email_subject,$email_body)) {
     echo "Mail Sent";
-}   else  {
+
     echo "Cannot Send";
-}
 
 ?>
